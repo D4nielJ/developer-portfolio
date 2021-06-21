6567149a6978d03b774dc232e096072a7d424084
arthurGC <aguzmanc@unsa.edu.pe>
2021-06-21 11:19:19 -0500

    Add openMenu function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const hamburguerIcon = document.querySelector('.menu-btn-open');
 const closeIcon = document.querySelector('.menu-btn-close');
+const menuMobile = document.querySelector('.menu-list-mobile');
 
 
 function openMenu() {
 	hamburguerIcon.classList.toggle('hidden');
 	closeIcon.classList.toggle('hidden');
+	menuMobile.classList.toggle('hidden');
 }
 
 hamburguerIcon.addEventListener('click',openMenu);
