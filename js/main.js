e564a0552edba099762842009328cfa3b6b24a05
D4nielJ <d4niel.djm@gmail.com>
2021-06-22 13:12:01 -0500

    Add JS for modals v1.0 Selectors, function openModal and events listeners

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 // Selectors
 
 const body = document.querySelector('body');
-
 const hamburguerIcon = document.querySelector('.menu-btn-open');
 const closeIcon = document.querySelector('.menu-btn-close');
 const menuMobile = document.querySelector('.menu-list');
@@ -21,3 +20,90 @@ function openMenu() {
 hamburguerIcon.addEventListener('click', openMenu);
 closeIcon.addEventListener('click', openMenu);
 menuMobileLi.forEach((Element) => Element.addEventListener('click', openMenu));
+
+// M O D A L S
+// Information for Modals through objects
+
+const modalInformation = [
+  {
+    title: 'Tonic',
+    company: 'Canopy',
+    role: 'Back End Dev',
+    year: '2015',
+    imgSrc: './media/snapshoot-portfolio.png',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam architecto sint, quae sit nostrum accusamus magnam blanditiis iusto in ducimus eligendi voluptatibus commodi.',
+    tech: [
+      'html',
+      'ruby on Rails',
+      'css',
+      'javaScript'
+    ]
+    liveHREF: 'https://d4nielj.github.io/developer-portfolio/',
+    sourceHREF 'https://github.com/D4nielJ/developer-portfolio',
+  }, {
+    title: 'Multi-Post Stories',
+    company: 'Facebook',
+    role: 'Full Stack Dev',
+    year: '2015',
+    imgSrc: './media/snapshoot-portfolio-1.png',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam architecto sint, quae sit nostrum accusamus magnam blanditiis iusto in ducimus eligendi voluptatibus commodi.',
+    tech: [
+      'html',
+      'ruby on Rails',
+      'css',
+      'javaScript'
+    ]
+    liveHREF: 'https://d4nielj.github.io/developer-portfolio/',
+    sourceHREF 'https://github.com/D4nielJ/developer-portfolio',
+  }, {
+    title: 'Facebook 360',
+    company: 'Facebook',
+    role: 'Full Stack Dev',
+    year: '2015',
+    imgSrc: './media/snapshoot-portfolio-2.png',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam architecto sint, quae sit nostrum accusamus magnam blanditiis iusto in ducimus eligendi voluptatibus commodi.',
+    tech: [
+      'html',
+      'ruby on Rails',
+      'css',
+      'javaScript'
+    ]
+    liveHREF: 'https://d4nielj.github.io/developer-portfolio/',
+    sourceHREF 'https://github.com/D4nielJ/developer-portfolio',
+  }, {
+    title: 'Uber Navigation',
+    company: 'Uber',
+    role: 'Lead Developer',
+    year: '2018',
+    imgSrc: './media/snapshoot-portfolio-3.png',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam architecto sint, quae sit nostrum accusamus magnam blanditiis iusto in ducimus eligendi voluptatibus commodi.',
+    tech: [
+      'html',
+      'ruby on Rails',
+      'css',
+      'javaScript'
+    ]
+    liveHREF: 'https://d4nielj.github.io/developer-portfolio/',
+    sourceHREF 'https://github.com/D4nielJ/developer-portfolio',
+  }
+]
+
+// Selectors for Modals
+
+const modal = document.querySelector('#modal');
+const modalWindow = document.querySelector('.modal__window');
+const buttonSeeProject = document.querySelectorAll('.work-card-seeProject');
+const buttonClose = document.querySelector('.modal__icon-close');
+
+// Function openModal
+
+function openModal() {
+  modal.classList.toggle('modal--active');
+  modalWindow.classList.toggle('modal__window--transition');
+  body.classList.toggle('overflow-hidden');
+}
+
+// Events
+
+buttonSeeProject.forEach((Element) => Element.addEventListener('click', openModal));
+buttonClose.addEventListener('click', openModal);
