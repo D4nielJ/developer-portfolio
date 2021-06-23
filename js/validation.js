9d8ab2a7b69bce53e35b8583ed4872687ca32703
arthurGC <aguzmanc@unsa.edu.pe>
2021-06-23 14:37:55 -0500

    Add validation function

--- js/validation.js
+++ js/validation.js
@@ -2,24 +2,21 @@ const form = document.querySelector('.contact-me-form');
 const email = document.querySelector('#user_email');
 const error = document.querySelector('.form__error-msg');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    error.classList.remove('form__error-msg--active')
-    error.classList.add('form__error-msg--active');
-})
+function isItUppercase(value) {
+    if (value === value.toLowerCase()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-// value1 = "mayus@gmail.com" // Should return match
-// value2 = "minus" // Should return null
-// value3 = "MiXeD" // Should return match
+// form.addEventListener('submit', (e) => {
 
-// function isItUppercase(value) {
-//   if (value.match(/^[a-z@.]*$/)) {
-//     return "Okay";
-//   } else {
-//     return "Not okay"
-//   }
-// }
-
-// console.log(isItUppercase(value1));
-// console.log(isItUppercase(value2));
-// console.log(isItUppercase(value3));
+//     if (isItUppercase(email.value)) {
+//         error.classList.remove('form__error-msg--active');
+//     }else {
+//         e.preventDefault();
+//         error.classList.add('form__error-msg--active');
+//     }
+    
+// })
